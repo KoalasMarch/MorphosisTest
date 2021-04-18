c50059abb11b78a0c774df0b8df9db325b8d9d0d
KoalasMarch <sasuke_kub@hotmail.com>
2021-04-19 06:32:12 +0700

    add active storage to product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -14,14 +14,17 @@ class Admin::ProductsController < Admin::BaseController
 
   def create
     @product = Product.new(product_params)
+    @product.image.attach(params[:product][:image]) if params[:product][:image].present?
     Stock.new(product: @product, stock_number: params[:stock_number]) if params[:stock_number].present?
     return render json: { errors: @product.errors.full_messages } unless @product.save
 
-    render status: :created
+    render json: { product: @product.as_json, image: @product.image_url }, status: :created
   end
 
   def update
+    @product.image.attach(params[:product][:image]) if params[:product][:image].present?
     return render json: { errors: @product.errors.full_message } unless @product.update(product_params)
+    render json: { product: @product.as_json, image: @product.image_url }
   end
 
   def destroy
@@ -36,6 +39,6 @@ class Admin::ProductsController < Admin::BaseController
     end
 
     def product_params
-      params.require(:product).permit(:title, :description, :price, :sku)
+      params.require(:product).permit(:title, :description, :price, :sku, :image)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -15,7 +15,12 @@ class Product < ApplicationRecord
   has_many :orders, through: :product_orders
   has_many :product_sets
   has_one :stock
+  has_one_attached :image
 
   validates :title, presence: true
   validates :price, presence: true
+
+  def image_url
+    Rails.application.routes.url_helpers.rails_blob_url(image, only_path: true)
+  end
 end
